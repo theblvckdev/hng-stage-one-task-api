64c706f1cc3acc7f5d4b7dbee5c8117096e4b487
theblvckdev <nathancodes05@gmail.com>
2024-07-02 14:21:31 +0100

    code update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // important if app is behind a proxy
 app.set("trust proxy", true);
